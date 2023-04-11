2fd7d1667b0739cbba57b86a32f3bb85f512501a
muhammed-nazim <88503917+muhammed-nazim@users.noreply.github.com>
2023-04-12 01:06:26 +0530

    Update generate_benign_trafic.py

--- IDS_in_IDS/mininet/generate_benign_trafic.py
+++ IDS_in_IDS/mininet/generate_benign_trafic.py
@@ -89,7 +89,7 @@ def startNetwork():
 
     print ("Starting Network")
     topo = MyTopo()
-    net = Mininet( topo=topo, host=CPULimitedHost, link=TCLink, controller=None )
+    #net = Mininet( topo=topo, host=CPULimitedHost, link=TCLink, controller=None )
     net.addController( 'c0', controller=RemoteController, ip='192.168.1.102', port=6653 )
 
     c0 = RemoteController('c0', ip='192.168.1.102', port=6653)
